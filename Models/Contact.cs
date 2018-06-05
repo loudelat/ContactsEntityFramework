fdf76e8f4d82d60f45740b7b336aa274a8ab422a
Unknown <loudelat@amazon.com>
2018-06-05 16:20:28 -0700

    Imported packages

---
+++ Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ContactsEntityFramework.Models
+{
+    public class Contact
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+        public string GetFullName()
+		{
+			return this.FirstName + " " + this.LastName;
+		}
+	}
+}
